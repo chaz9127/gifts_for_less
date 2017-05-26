0b4c8f344b07212abf7bab32330fb4b6f84e37f7
CJ <cj@gamersensei.com>
2017-05-26 00:37:18 -0400

    basic front end wireframe

--- app/assets/javascripts/angular/controllers/HomeCtrl.js
+++ app/assets/javascripts/angular/controllers/HomeCtrl.js
@@ -4,6 +4,29 @@ angular.module('GiftsForLess')
 HomeCtrl.$inject = ['$scope', '$state'];
 
 function HomeCtrl($scope, $state) {
-  $scope.state = $state.current.name;
-  console.log($scope.state);
+  $scope.holidays = ['All', 'Mother\'s Day', 'Father\'s Day'];
+  $scope.selectedHoliday = 'All';
+  $scope.price = 15;
+  $scope.selectedSort = 'newest';
+
+  $scope.items = [
+    {
+      title: 'Title',
+      description: 'This should be much longer text.This should be much longer text.This should be much longer text.This should be much longer text.',
+      price: 15.58,
+      link: 'https://www.amazon.com'
+    },
+    {
+      title: 'This Foo',
+      description: 'This should be much longer text.much longer text. should be much should be much. is should be much longer tex.',
+      price: 82.58,
+      link: 'https://www.amazon.com'
+    },
+    {
+      title: 'A Bar over here',
+      description: 'This should be much longer text.',
+      price: 12.58,
+      link: 'https://www.amazon.com'
+    }
+  ]
 }
