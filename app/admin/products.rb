b73f112eed490fbf2cd57fa1bae05f4dca8ff6f1
CJ <cj@gamersensei.com>
2017-05-31 11:17:30 -0400

    add ability to add/view tags for a product

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,4 +1,26 @@
 ActiveAdmin.register Product do
+
+  member_action :edit_add_tags do
+    @product = Product.find(params['id'])
+  end
+
+  member_action :add_tags, method: :put do
+    begin
+      product = Product.find(params[:update_params][:id])
+      holiday_tags = params[:update_params][:holiday]
+      genre_tags = params[:update_params][:genre]
+
+      product.holiday_list.add(holiday_tags, parse: true)
+      product.genre_list.add(genre_tags, parse: true)
+
+      product.save!
+
+      redirect_to resource_path(params[:update_params][:id]), notice: "Tags successfuly added"
+    rescue => exception
+      redirect_to resource_path(params[:update_params][:id]), flash: { error: exception.to_s }
+    end
+  end
+
   controller do
     def permitted_params
       params.permit product: [:name, :description, :affiliate_link, :price, :image]
@@ -30,13 +52,21 @@ ActiveAdmin.register Product do
   end
 
   show do |product|
+    panel 'Add Tags' do
+      div { link_to 'Add Tags', edit_add_tags_admin_product_path(product.id) }
+    end
+
     attributes_table do
       row :name
       row :description
       row :affiliate_link
       row :price
-      row :genres
-      row :holidays
+      row :genres do
+        product.genre_list.to_s
+      end
+      row :holidays do
+        product.holiday_list.to_s
+      end
       row :image do
         tag("img", src: product.image.url)
       end
